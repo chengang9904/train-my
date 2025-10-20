58dee9e3d0f21c3fc9b807b6b44d19b182a1c843
chengang <chengang9904@163.com>
2025-10-20 19:38:47 +0800

    feat(gateway): 初始化网关服务并配置路由转发
    - 添加网关模块并引入 Spring Cloud Gateway 依赖
    - 配置应用端口为 8000 并设置路由规则
    - 实现将 /member/** 请求转发至 http://127.0.0.1:8001
    - 更新 member 服务端口为 8001 并调整上下文路径
    - 修改测试接口地址以适配新的网关入口- 配置 logback 日志输出格式及文件存储路径- 启动类添加包扫描和日志打印功能

---
+++ gateway/src/main/java/com/jiawa/train/gateway/config/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.jiawa.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+/**
+ * @Author: chengang
+ * @ClassName: GatewayApplication
+ * @Version: v1.0.0
+ * @Description:
+ **/
+
+@SpringBootApplication
+@ComponentScan("com.jiawa")
+public class GatewayApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
