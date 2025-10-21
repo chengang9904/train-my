12f72ce3f532518e7a9faa70989a004639b37e26
chengang <chengang9904@163.com>
2025-10-21 20:19:20 +0800

    feat(web): 配置开发与生产环境及优化登录流程- 添加 .env.dev 和 .env.prod 环境配置文件 - 修改 LoginView 组件图标命名以符合新版本规范 - 移除登录成功后的路由跳转与状态存储逻辑- 在 main.js 中配置 axios 拦截器并设置默认 baseURL - 更新 package.json 的 serve 脚本为区分环境的命令

--- web/src/main.js
+++ web/src/main.js
@@ -6,6 +6,7 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 // 引入所有图标
 import * as Icons from "@ant-design/icons-vue";
+import axios from "axios";
 
 const app = createApp(App);
 
@@ -15,3 +16,23 @@ const icons = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境：', process.env.VUE_APP_SERVER);
--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <a-row class="login">
     <a-col :span="8" :offset="8" class="login-main">
-      <h1 style="text-align: center"><rocket-two-tone />&nbsp;甲蛙12306售票系统</h1>
+      <h1 style="text-align: center"><RocketTwoTone />&nbsp;甲蛙12306售票系统</h1>
       <a-form
           :model="loginForm"
           name="basic"
@@ -40,14 +40,10 @@
 <script>
 import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
-import { notification } from 'ant-design-vue';
-import { useRouter } from 'vue-router'
-import store from "@/store";
 
 export default defineComponent({
   name: "login-view",
   setup() {
-    const router = useRouter();
 
     const loginForm = reactive({
       mobile: '13000000000',
@@ -60,25 +56,14 @@ export default defineComponent({
       }).then(response => {
         let data = response.data;
         if (data.success) {
-          notification.success({ description: '发送验证码成功！' });
           loginForm.code = "8888";
-        } else {
-          notification.error({ description: data.message });
         }
       });
     };
 
     const login = () => {
       axios.post("/member/member/login", loginForm).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({ description: '登录成功！' });
-          // 登录成功，跳到控台主页
-          router.push("/welcome");
-          store.commit("setMember", data.content);
-        } else {
-          notification.error({ description: data.message });
-        }
+        console.log(response);
       })
     };
 
